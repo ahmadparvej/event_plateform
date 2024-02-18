aa445ff2ed9159c51cc5c5276dade7b6dec935e2
ahmadparvej <ahmadparvej58@gmail.com>
2024-02-18 13:02:45 +0530

    import bug fix

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -2,7 +2,7 @@ import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
-import { clerkClient } from '@/node_modules/@clerk/nextjs/dist/types/server-helpers.server'
+import { clerkClient } from '@clerk/nextjs'
 import { NextResponse } from '@/node_modules/next/server'
  
 export async function POST(req: Request) {
