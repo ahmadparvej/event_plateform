de59d70253879786b38b6418de9d52530ae55d56
ahmadparvej <ahmadparvej58@gmail.com>
2024-02-18 13:22:54 +0530

    import Document from mongoose

--- lib/database/models/category.model.ts
+++ lib/database/models/category.model.ts
@@ -1,14 +1,14 @@
-import { Schema, models, model } from 'mongoose';
+import { Schema, Document, models, model } from 'mongoose';
 
 export interface ICategory extends Document {
     _id: String,
     name: String
 }
 
-const CategorySchema = new Schema({
+const CategorySchema = new Schema<ICategory>({
     name: { type: String, required: true, unique: true }
 })
 
-const Category = models.Category || model('Category', CategorySchema)
+const Category = models.Category || model<ICategory>('Category', CategorySchema)
 
 export default Category
